60bc1c7564ceaee5fd8b6c7b047248f2680f52f3
kiragermanica <93350652+kiragermanica@users.noreply.github.com>
2021-11-16 11:38:50 +0300

    sub_domains_enumeration.py

---
+++ sub_domains_enumeration.py
@@ -0,0 +1,17 @@
+import requests
+import sys
+
+sub_list = open("/path_to_wordlist").read()
+subdoms = sub_list.splitlines()
+
+for sub in subdoms:
+    sub_domains = f"http://{sub}.{sys.argv[1]}"
+
+    try:
+        requests.get(sub_domains)
+
+    except requests.ConnectionError:
+        pass
+
+    else:
+        print("Valid domain: ", sub_domains)
